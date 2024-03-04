f0e49c1cb4aed6173c29ab9687fedda4afdfc91c
Hugo Leung <132837475+hugounique@users.noreply.github.com>
2024-03-04 22:23:28 +0800

    Finish tab-switiching logic

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-// import printHomePageContent from './home';
+import printHomePageContent from './home';
+import printMenuContent from './menu';
 import './styles.css';
 
+const container = document.querySelector('#content');
 const navButtons = document.querySelectorAll('.nav-tab');
 
-const container = document.querySelector('#content');
+for (let i = 0; i < navButtons.length; i++) {
+    navButtons[i].dataset.id = i;
+    navButtons[i].addEventListener('click', renderPageContent);
+}
+
+function renderPageContent (e) {
+    container.innerHTML = "";
+    if (e.target.dataset.id === "0") {
+        container.appendChild(printHomePageContent());
+    }
+    if (e.target.dataset.id === "1") {
+        container.appendChild(printMenuContent());
+    }
+};
 
 
-container.appendChild(printHomePageContent());
