cc1494597fd3d7c1187e88f5c5f7d3e49c76b2f4
Hugo Leung <132837475+hugounique@users.noreply.github.com>
2024-03-05 10:56:29 +0800

    Import custom japanese font

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,13 @@ module.exports = {
                     filename: 'images/[name][ext]'
                 },
             },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: '[name].[ext]'
+                },
+            },
         ],
     },
 };
